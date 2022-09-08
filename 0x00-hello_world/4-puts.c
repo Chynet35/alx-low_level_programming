4dd1c68b92d849f323d48a5d1a3a1ceb4d1433ae
Chioma Enemuo <enemuochioma77@gmail.com>
2022-09-08 16:40:05 -0700

    Tasks 4-6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - C program that prints
+*
+* Return: Always 0 (Successful)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual
+puzzle");
+return (0);
+}
