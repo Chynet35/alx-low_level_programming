9365ab17027168ef1c0b2f71d23131b9ce3b900c
Chioma Enemuo <enemuochioma77@gmail.com>
2022-09-08 19:10:25 -0700

    Multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,8 @@
 /**
 * main - C program that prints
 *
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
 * Return: Always 0 (Successful)
 */
 int main(void)
