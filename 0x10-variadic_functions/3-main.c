7e3f44165c822b5cef82119754700610150dd815
Chioma Enemuo <enemuochioma77@gmail.com>
2022-10-13 08:15:14 -0700

    Sum of parameters

---
+++ 0x10-variadic_functions/3-main.c
@@ -0,0 +1,14 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+	print_all("ceis", 'H', 0, "lberton");
+	print_all("fcsi", 3.14435, 'H', NULL, 402);
+	return (0);
+}
